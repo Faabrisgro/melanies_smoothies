7b8351ef591c88b5694bb48ffeb1aae3a2fa4ed4
Fabrizio Sgro <117785876+Faabrisgro@users.noreply.github.com>
2024-04-20 19:21:22 -0300

    Update streamlit_app.py
    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,8 +28,9 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
-    #st.write(ingredients_string)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
+        #st.write(ingredients_string)
 
     my_insert_stmt = """ 
                         insert into smoothies.public.orders 
@@ -43,7 +44,4 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered'+ ', '+ name_on_order + '!', icon= "✅")
 
-# New Secion
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response)
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
+
